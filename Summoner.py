2ca5bf12179045bc8657dbe68133c8f0815671ca
zadaki <61480852+zadaki@users.noreply.github.com>
2020-03-04 21:36:17 -0500

    Update Summoner.py

--- Summoner.py
+++ Summoner.py
@@ -1,8 +1,4 @@
 #Imports the packages necessary.
-#Requests and JSON are here because 
-#I encapsulated the API calls to the methods where
-#they are made. No more random URLs lying around 
-#in the main code, just the API key. 
 import requests, json;
 
 class Summoner:
@@ -13,58 +9,46 @@ class Summoner:
 		url='https://na1.api.riotgames.com/lol/summoner/v4/summoners/by-name/'+sum_name+'?api_key='+api_key;
 		getInfo = requests.get(url);
 		data = getInfo.json();
-		dump = json.dumps(data);
 
-		#Python gets wacky here because you don't have to declare instance
-		#variables for classes/objects like you do in Java. Instead, you use
-		#the self. to designate that you are assigning a value to the variables
-		#that belong to the Summoner object. Here, we're just storing the player
-		#data that we used to have in the main program.
 		self.sum_name = sum_name;
 		self.sum_icon = data['profileIconId'];
 		self.acc_id = data['accountId'];
 		self.acc_lvl = data['summonerLevel'];
+		self.eId = data['id'];
 
-	#This method is super stupid.
-	#This lets it be called. May not be necessary.
-	def __call__(self):
-		pass;
+		del data, url, getInfo;
 
-	#Quick little method that returns the level
-	#as a string instead of a long. Was originally
-	#for the printSummoner method, but may be useful.
-	#It can be moved there if not.
+		url = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{0}/?api_key={1}'.format(self.eId, api_key);
+		response = requests.get(url);
+		data = response.json();
+
+		data = {value['queueType']: value for value in data};
+		data.get('RANKED_SOLO_5x5', None);
+
+		data2 = data['RANKED_SOLO_5x5'];
+
+		self.wins = data2['wins'];
+		self.losses = data2['losses'];
+		self.tier = data2['tier'];
+		self.rank = data2['rank'];
+		self.lp = data2['leaguePoints'];
+		self.leagueId = data2['leagueId']
+		self.streak = data2['hotStreak'];
+		self.inactive = data2['inactive'];
+
 	def levelString(self):
 		lvlString = str(self.acc_lvl);
 		return lvlString;
 
-	#This method displays the player information as we
-	#currently have it in a nicely formatted string to 
-	#the command line.
 	def printSummoner(self):
 		lvl = self.levelString();
-		print("\nSummoner Name: {0}\nLevel: {1}\nAccount ID: {2}\nSummoner Icon: {3}\n".format(self.sum_name, self.acc_lvl, self.acc_id, self.sum_icon));
+		wr = str(round(100*(self.wins/(self.losses+self.wins))))+'%';
+		rankString = self.tier+" "+self.rank+"\n"+str(self.wins)+"W "+str(self.losses)+'L   '+str(self.lp)+" LP\n"+wr;
+		statString = self.sum_name+"'s Stats";
 
-	#Generates the match list for the player when called.
-	#Takes in the API key as a parameter to send the query
-	#to the API and return the data.
+		print(statString+'\n'+'\nLevel '+str(self.acc_lvl)+'\n'+rankString)
+
 	def makeMatchList(self, api_key):
 		matchlist = requests.get('https://na1.api.riotgames.com/lol/match/v4/matchlists/by-account/' + self.acc_id + '?api_key=' + api_key);
 		matchlistJson = matchlist.json();
 		return matchlistJson;
-	
-	def getSeasonStats(self, api_key):
-		url = 'https://na1.api.riotgames.com/lol/league/v4/entries/by-summoner/{0}/?api_key={1}'.format(self.eId, api_key);
-		grabStats = requests.get(url);
-
-		#This grabs the API pull for Summoner ranked informatio and displays the file type after using .json() method call and displays the file.
-		data = grabStats.json();
-		print(type(data));
-		print(data);
-
-		#This commented segment is the converting method I've been using that works, but returns it as an empty dictionary. Ignore it for now.
-		#it = iter(data);
-		#convert = dict(zip(it, it));
-
-		#print(convert);
-		#print(type(convert));
